880b11b260a1bf8805d50b43bf4e7249bbdfcbbb
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-08-31 09:10:35 +0300

    Demo01

--- Demo01/Configuration/EmployeeConfiguration.cs
+++ Demo01/Configuration/EmployeeConfiguration.cs
@@ -36,7 +36,6 @@ namespace Demo01.Configuration
                  *   .HasDefaultValue(DateTime.Now);
                  */
 
-
                 E.Property(e => e.DateOfCreation)
                  .HasDefaultValueSql("GETDATE()");
 
